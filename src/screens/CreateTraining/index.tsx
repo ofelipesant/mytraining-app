1a2de4fb02c296f2b2f6cdc4f1a89ac382c9dc94
felipesantosdooca <ofelipe.dev@gmail.com>
2023-01-17 22:03:20 -0300

    wip: tela de criação de treino

--- src/screens/CreateTraining/index.tsx
+++ src/screens/CreateTraining/index.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { Container } from "./styles";
-import { View, Text, StyleSheet } from "react-native"
+import { Container, Input, RegisterContainer, ExerciseRegisterCard } from "./styles";
+import { View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native"
 import ScreenTitle from "../../components/ScreenTitle";
 
 export default function CreateTraining(){
     return (
-        <Container>
-            <ScreenTitle title="Novo Treino" backIcon={true}/>
-            <Text>Tela de criação de treinos</Text>
-        </Container>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Container>
+                <ScreenTitle title="Novo Treino" backIcon={true}/>
+
+                    <RegisterContainer>
+                        <Input
+                            placeholder="Nome do treino"
+                            placeholderTextColor={"#b8b8b8"}
+                        />
+
+                        <ExerciseRegisterCard>
+                            <View>
+                                <TextInput
+                                    placeholder="Nome do exercício"
+                                    placeholderTextColor={"#b8b8b8"}
+                                />
+                                <TextInput
+                                    placeholder="Peso"
+                                    keyboardType="numeric"
+                                    placeholderTextColor={"#b8b8b8"}
+                                />
+                                <TextInput
+                                    placeholder="Séries"
+                                    keyboardType="numeric"
+                                    placeholderTextColor={"#b8b8b8"}
+                                />
+                                <TextInput
+                                    placeholder="Repetições"
+                                    keyboardType="numeric"
+                                    placeholderTextColor={"#b8b8b8"}
+                                />
+                            </View>
+                        </ExerciseRegisterCard>
+                    </RegisterContainer>
+                
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
--- src/screens/CreateTraining/styles.ts
+++ src/screens/CreateTraining/styles.ts
@@ -7,3 +7,29 @@ export const Container = styled.View`
     height: 100%;
     padding: 12px;
 `
+
+export const RegisterContainer = styled.View`
+    margin-top: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Input = styled.TextInput`
+    width: 100%;
+    height: 40px;
+    color: white;
+    background-color: #3D3D3D;
+    border-radius: 4px;
+    font-size: 18px;
+    padding: 4px;
+    margin-bottom: 16px;
+`
+
+export const ExerciseRegisterCard = styled.View`
+    background-color: #3D3D3D;
+    width: 100%;
+    height: 124px;
+    border-radius: 4px;
+    margin: 8px 0;
+`
